971c4a90da3765347545f3c4cf541d8d0af8ae3b
Collin Richards <richardscollin@gatech.edu>
2024-06-04 20:05:19 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-mod gitlab;
-mod rustfmt;
-
 use std::{
     self,
     fs::File,
@@ -200,6 +197,160 @@ fn main() {
         Command::Stats(args) => {
             let output = get_outfile(args.output.as_ref());
             stats(&args, output).unwrap();
+        }
+    }
+}
+
+mod gitlab {
+
+    use std::hash::Hasher;
+
+    use cargo_metadata::{diagnostic::DiagnosticLevel, CompilerMessage};
+    use serde::{Deserialize, Serialize};
+
+    /// <https://docs.gitlab.com/ee/ci/testing/code_quality.html#implement-a-custom-tool>
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct CodeQualityReportEntry {
+        description: String,
+        check_name: String,
+        fingerprint: String,
+        severity: Severity,
+        location: Location,
+    }
+
+    impl CodeQualityReportEntry {
+        pub fn new(
+            check_name: String,
+            severity: Severity,
+            description: String,
+            filename: String,
+            lineno: usize,
+        ) -> Self {
+            let fingerprint = {
+                #[allow(deprecated)]
+                let mut hasher = std::hash::SipHasher::new();
+                hasher.write(filename.as_bytes());
+                hasher.write_u8(0xff);
+                hasher.write(description.as_bytes());
+                format!("{:x}", hasher.finish())
+            };
+
+            Self {
+                description,
+                check_name,
+                fingerprint,
+                severity,
+                location: Location {
+                    path: filename,
+                    lines: Lines { begin: lineno },
+                },
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, Debug, Deserialize, Serialize)]
+    #[serde(rename_all = "lowercase")]
+    pub enum Severity {
+        Info,
+        Minor,
+        Major,
+        Critical,
+        Blocker,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    struct Location {
+        path: String,
+        lines: Lines,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    struct Lines {
+        begin: usize,
+    }
+
+    impl TryFrom<CompilerMessage> for CodeQualityReportEntry {
+        type Error = ();
+
+        fn try_from(value: CompilerMessage) -> Result<Self, Self::Error> {
+            let diagnostic = value.message;
+            let description = diagnostic.message;
+
+            let span = diagnostic.spans.first().ok_or(())?.to_owned();
+            let path = span.file_name;
+            let begin = span.line_start;
+            let span_text = span
+                .text
+                .iter()
+                .map(|line| line.text.trim())
+                .collect::<String>();
+
+            Ok(Self::new(
+                diagnostic
+                    .code
+                    .map(|dc| dc.code)
+                    .unwrap_or(String::from("unknown")),
+                diagnostic.level.try_into()?,
+                format!("{description}. {span_text}"),
+                path,
+                begin,
+            ))
+        }
+    }
+
+    impl TryFrom<DiagnosticLevel> for Severity {
+        type Error = ();
+
+        fn try_from(value: DiagnosticLevel) -> Result<Self, Self::Error> {
+            Ok(match value {
+                DiagnosticLevel::Note | DiagnosticLevel::Help => Self::Info,
+                DiagnosticLevel::Error => Self::Major,
+                DiagnosticLevel::Warning => Self::Minor,
+                DiagnosticLevel::Ice | DiagnosticLevel::FailureNote => return Err(()),
+                _ => return Err(()),
+            })
+        }
+    }
+}
+
+mod rustfmt {
+
+    use std::{borrow::Cow, marker::PhantomData};
+
+    use serde::Deserialize;
+
+    use crate::gitlab::{CodeQualityReportEntry, Severity};
+
+    #[derive(Clone, Debug, Deserialize)]
+    struct RustfmtJsonEntry<'a> {
+        /// full path filename
+        name: Cow<'a, str>,
+        mismatches: Vec<Mismatch<'a>>,
+    }
+
+    #[derive(Clone, Debug, Deserialize)]
+    struct Mismatch<'a> {
+        original_begin_line: usize,
+        // original_end_line: usize,
+        // expected_begin_line: usize,
+        // expected_end_line: usize,
+        // original: Cow<'a, str>,
+        // expected: Cow<'a, str>,
+        #[serde(skip)]
+        _phantom: PhantomData<&'a ()>,
+    }
+
+    impl TryFrom<RustfmtJsonEntry<'_>> for CodeQualityReportEntry {
+        type Error = ();
+
+        fn try_from(value: RustfmtJsonEntry) -> Result<Self, Self::Error> {
+            Ok(Self::new(
+                "rustfmt".to_string(),
+                Severity::Minor,
+                "".to_string(),
+                value.name.to_string(),
+                value.mismatches.first().ok_or(())?.original_begin_line,
+            ))
         }
     }
 }
